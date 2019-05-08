9a82c9e64eb0f57037fceb3aed0aa5aa82eca324
Sebastian <sanctuary@yandex.ru>
2019-05-08 12:51:33 +0500

    initial

--- mySweep/Form1.Designer.cs
+++ mySweep/Form1.Designer.cs
@@ -28,12 +28,229 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(8, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Размер клетки";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Lavender;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.ForeColor = System.Drawing.Color.Red;
+            this.textBox1.Location = new System.Drawing.Point(103, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(38, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "20";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Lavender;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.ForeColor = System.Drawing.Color.Red;
+            this.textBox2.Location = new System.Drawing.Point(103, 34);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(38, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "30";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(55, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Строк";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.Lavender;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox3.ForeColor = System.Drawing.Color.Red;
+            this.textBox3.Location = new System.Drawing.Point(103, 83);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(20, 20);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.Text = "15";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(64, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Мин";
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.Lavender;
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox4.ForeColor = System.Drawing.Color.Red;
+            this.textBox4.Location = new System.Drawing.Point(103, 59);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(38, 20);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.Text = "20";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(37, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Столбцов";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lavender;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.button1.Location = new System.Drawing.Point(11, 109);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 27);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "ГО";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.checkBox1.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox1.ForeColor = System.Drawing.Color.White;
+            this.checkBox1.Location = new System.Drawing.Point(11, 142);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(112, 20);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "режим отладки";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.checkBox2.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox2.ForeColor = System.Drawing.Color.White;
+            this.checkBox2.Location = new System.Drawing.Point(11, 163);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(86, 20);
+            this.checkBox2.TabIndex = 11;
+            this.checkBox2.Text = "помощник";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(8, 186);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 16);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Мин нет";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(8, 202);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 16);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Флагов нет";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(124, 85);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(17, 16);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "%";
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ClientSize = new System.Drawing.Size(153, 230);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- mySweep/Form1.cs
+++ mySweep/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Web;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,324 @@ namespace mySweep
 {
     public partial class Form1 : Form
     {
+        //private const int size = 20;//размер клетки
+        //private const int rows = 30;//строк
+        //private const int cols = 50;//столбцов
+        //private const int minePer = 15;//% мин
+        private int[,] theSea;
+        private bool[,] isChecked;
+        private int recursionlvl, mines, flags;
+        private int size;//размер клетки
+        private int rows;//строк
+        private int cols;//столбцов
+        private int minePer;//% мин
+        private bool debug = false;
+        private bool helper = true;
+        private bool zeroHit;
+
         public Form1()
         {
             InitializeComponent();
+            checkBox2.Checked = false;
+            checkBox2.Enabled = false;     
+        }
+
+        private void setup(int size, int rows, int cols, int minePer)
+        {
+            this.Size = new Size(size * cols + 150, size * rows);
+            this.CenterToScreen();
+            //Image myimage = new Bitmap(@"C:\Users\Себастиан\Documents\Visual Studio 2015\Projects\mySweep\mySweep\bin\Release\satskwp3.jpg");
+            //this.BackgroundImage = myimage;
+            //this.BackgroundImageLayout = ImageLayout.Stretch;
+
+            mines = 0;
+            flags = 0;
+            zeroHit = false;
+
+            int left = 150, top = 0;
+            theSea = new int[rows, cols];
+            Random rand = new Random();
+
+            for (int i = 0; i < rows; i++)
+                for (int j = 0; j < cols; j++)
+                {
+                    if (rand.Next(0, 100) < minePer)
+                    {
+                        theSea[i, j] = 1;
+                        mines++;
+                    }
+                    else
+                        theSea[i, j] = 0;
+                }
+
+
+            for (int i = 0; i < rows; i++)
+                for (int j = 0; j < cols; j++)
+                {
+                    //создание кнопок
+                    Button button = new Button();
+                    button.Text = " ";
+                    button.Left = left + size * j;
+                    button.Top = top + size * i;
+                    button.BackColor = System.Drawing.Color.CornflowerBlue;
+                    button.FlatAppearance.BorderColor = System.Drawing.Color.White;
+                    button.FlatAppearance.BorderSize = 0;
+                    button.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Lavender;
+                    button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.WhiteSmoke;
+                    button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+                    button.Name = "b" + i.ToString().PadLeft(5, '0') + j.ToString().PadLeft(5, '0');
+                    button.Size = new System.Drawing.Size(size, size);
+                    button.TabIndex = 0;
+                    button.ForeColor = Color.White;
+                    button.Font = new Font("CenturyGothic", (float)(size / 2.5), FontStyle.Bold);
+                    //debug
+                    if (debug == true && theSea[i, j] == 1) button.Text = "!";
+                    this.Controls.Add(button);
+                    button.MouseDown += new MouseEventHandler(this.MyButtonHandler);
+                }
+
+            label5.Text = "Мин: " + mines;
+            label6.Text = "Флагов: " + flags;
+        }
+
+        private void MyButtonHandler(object sender, MouseEventArgs e)
+        {
+            Control btn = (Control)sender;
+            if (e.Button == MouseButtons.Left)
+            {
+                recursionlvl = 0;
+                isChecked = new bool[rows, cols];
+                for (int i = 0; i < rows; i++)
+                    for (int j = 0; j < cols; j++)
+                        isChecked[i, j] = false;
+
+                btn.BackColor = System.Drawing.Color.Lavender;
+                for (int i = 0; i < rows; i++)
+                    for (int j = 0; j < cols; j++)
+                        if (btn.Name == ("b" + i.ToString().PadLeft(5, '0') + j.ToString().PadLeft(5, '0')))
+                            check(btn, i, j, true);
+            }
+            else
+            {
+                if (btn.Text == " " || btn.Text == "?" || btn.Text == "!")
+                {
+                    if (btn.Text == "?")
+                    {
+                        btn.Text = " ";
+                        btn.ForeColor = Color.White;
+                        //for (int i = 0; i < rows; i++)
+                        //    for (int j = 0; j < cols; j++)
+                        //        if (btn.Name == ("b" + i.ToString().PadLeft(5, '0') + j.ToString().PadLeft(5, '0')))
+                        //            if (theSea[i, j] == 1)
+                        //                mines++;
+                    }
+                    else
+                    {
+                        btn.ForeColor = Color.Red;
+                        btn.Text = "?";
+                        //for (int i = 0; i < rows; i++)
+                        //    for (int j = 0; j < cols; j++)
+                        //        if (btn.Name == ("b" + i.ToString().PadLeft(5, '0') + j.ToString().PadLeft(5, '0')))
+                        //            if (theSea[i, j] == 1)
+                        //                mines--;
+                    }
+                }
+            }
+
+            if (winCheck() == true)
+                MessageBox.Show("Все чисто!", "ГГВП");
+        }
+
+        private int check(Control btn, int i, int j, bool isClick)
+        {
+            recursionlvl++;
+            if (theSea[i, j] == 1 && isClick == true)
+            {
+                if (zeroHit == false)
+                {
+                    MessageBox.Show("Пока не стоит сюда кликать", "ОП");
+                    btn.BackColor = Color.CornflowerBlue;
+                    return -1;
+                }
+                else
+                {
+                    btn.BackColor = System.Drawing.Color.Red;
+                    btn.ForeColor = System.Drawing.Color.White;
+                    btn.Text = "X";
+                    DialogResult dialogResult = MessageBox.Show("Заново?", "ПОТРАЧЕНО", MessageBoxButtons.YesNo);
+                    if (dialogResult == DialogResult.Yes)
+                        Reset();
+                    return -1;//код проигрыша
+                }
+            }
+            else if (theSea[i, j] == 1 && isClick == false)
+                return -2;//код остановки рекурсии
+
+            //вычисление числа окружающих клетку мин
+            int mCounter = 0;
+            for (int m = i - 1; m < i + 2; m++)
+                for (int n = j - 1; n < j + 2; n++)
+                    if (m >= 0 && n >= 0 && m < rows && n < cols)
+                        if (theSea[m, n] == 1)
+                            mCounter++;
+            if (mCounter != 0)
+            {
+                btn.Text = mCounter.ToString();
+                switch (mCounter) {
+                    case 1:
+                        btn.ForeColor = Color.HotPink;
+                        break;
+                    case 2:
+                        btn.ForeColor = Color.Green;
+                        break;
+                    case 3:
+                        btn.ForeColor = Color.Blue;
+                        break;
+                    case 4:
+                        btn.ForeColor = Color.Red;
+                        break;
+                    case 5:
+                        btn.ForeColor = Color.Red;
+                        break;
+                    case 6:
+                        btn.ForeColor = Color.Red;
+                        break;
+                    case 7:
+                        btn.ForeColor = Color.Black;
+                        break;
+                    case 8:
+                        btn.ForeColor = Color.Black;
+                        break;
+                }
+
+                if (btn.BackColor == Color.CornflowerBlue && helper == true)
+                {
+                    int hCounter = 0;
+                    for (int m = i - 1; m < i + 2; m++)
+                        for (int n = j - 1; n < j + 2; n++)
+                            if (m >= 0 && n >= 0 && m < rows && n < cols)
+                                if (this.Controls["b" + m.ToString().PadLeft(5, '0') + n.ToString().PadLeft(5, '0')].Text == " " && this.Controls["b" + m.ToString().PadLeft(5, '0') + n.ToString().PadLeft(5, '0')].BackColor == Color.CornflowerBlue)
+                                    hCounter++;
+                    //MessageBox.Show(hCounter + " " + mCounter, "debug");
+                    if (hCounter == mCounter)
+                        for (int m = i - 1; m < i + 2; m++)
+                            for (int n = j - 1; n < j + 2; n++)
+                                if (m >= 0 && n >= 0 && m < rows && n < cols)
+                                    if (this.Controls["b" + m.ToString().PadLeft(5, '0') + n.ToString().PadLeft(5, '0')].Text == " " && this.Controls["b" + m.ToString().PadLeft(5, '0') + n.ToString().PadLeft(5, '0')].BackColor == Color.CornflowerBlue)
+                                        this.Controls["b" + m.ToString().PadLeft(5, '0') + n.ToString().PadLeft(5, '0')].BackColor = Color.LawnGreen;
+                }
+            }
+            else
+            {
+                zeroHit = true;
+                for (int m = i - 1; m < i + 2; m++)
+                    for (int n = j - 1; n < j + 2; n++)
+                        if (m >= 0 && n >= 0 && m < rows && n < cols)
+                            if (isChecked[m, n] == false)
+                            {
+                                isChecked[m, n] = true;
+                                check(this.Controls["b" + m.ToString().PadLeft(5, '0') + n.ToString().PadLeft(5, '0')], m, n, false);
+                                this.Controls["b" + m.ToString().PadLeft(5, '0') + n.ToString().PadLeft(5, '0')].BackColor = System.Drawing.Color.Lavender;
+                            }
+            }
+
+            for (int m = i - 1; m < i + 2; m++)
+                for (int n = j - 1; n < j + 2; n++)
+                    if (m >= 0 && n >= 0 && m < rows && n < cols)
+                        if (justACheck(m, n) == 0)
+                            if (isChecked[m, n] == false)
+                            {
+                                isChecked[m, n] = true;
+                                check(this.Controls["b" + m.ToString().PadLeft(5, '0') + n.ToString().PadLeft(5, '0')], m, n, false);
+                                this.Controls["b" + m.ToString().PadLeft(5, '0') + n.ToString().PadLeft(5, '0')].BackColor = System.Drawing.Color.Lavender;
+                            }
+
+            return mCounter;
+        }
+
+        private int justACheck(int i, int j)
+        {
+            int minesC = 0;
+            for (int m = i - 1; m < i + 2; m++)
+                for (int n = j - 1; n < j + 2; n++)
+                    if (m >= 0 && n >= 0 && m < rows && n < cols)
+                        if (theSea[m, n] == 1)
+                            minesC++;
+            return minesC;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+
+        private void Reset()
+        {
+            //label1.Visible = false;
+            //label2.Visible = false;
+            //label3.Visible = false;
+            //label4.Visible = false;
+            //textBox1.Visible = false;
+            //textBox2.Visible = false;
+            //textBox3.Visible = false;
+            //textBox4.Visible = false;
+            //button1.Visible = false;
+            //checkBox1.Visible = false;
+            try
+            {
+                size = Int32.Parse(textBox1.Text);
+                rows = Int32.Parse(textBox2.Text);
+                cols = Int32.Parse(textBox4.Text);
+                minePer = Int32.Parse(textBox3.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Введи нормальное число", "НЕ НОРМ");
+            }
+            if (checkBox1.Checked == true) debug = true;
+            else debug = false;
+            if (checkBox2.Checked == true) helper = true;
+            else helper = false;
+            this.Controls.Clear();
+            this.InitializeComponent();
+            setup(size, rows, cols, minePer);
+            checkBox1.Checked = debug;
+            checkBox2.Checked = helper;
+            checkBox2.Checked = false;
+            checkBox2.Enabled = false;
+            textBox1.Text = size.ToString();
+            textBox2.Text = rows.ToString();
+            textBox3.Text = minePer.ToString();
+            textBox4.Text = cols.ToString();
+        }
+
+        private bool winCheck()
+        {
+            int fCount = 0, fRight = 0;
+            for (int i = 0; i < rows; i++)
+                for (int j = 0; j < cols; j++)
+                    if (this.Controls["b" + i.ToString().PadLeft(5, '0') + j.ToString().PadLeft(5, '0')].Text == "?")
+                    {
+                        fCount++;
+                        flags = fCount;
+                        label6.Text = "Флагов: " + flags;
+                        if (theSea[i, j] == 1)
+                            fRight++;
+                    }
+            if (fCount == fRight && fRight == mines)
+                return true;
+            else
+                return false;
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.Escape)
+            {
+                this.Close();
+                return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
         }
     }
 }
